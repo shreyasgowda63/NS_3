1af7552b9a22049c220734a1fe16887c66320341
Davide Magrin <magrin.davide@gmail.com>
2019-10-14 11:58:22 +0200

    Enable log components for new classes

--- src/lte/examples/lena-deactivate-bearer.cc
+++ src/lte/examples/lena-deactivate-bearer.cc
@@ -76,6 +76,9 @@ main (int argc, char *argv[])
   LogComponentEnable ("LteHelper", logLevel);
   LogComponentEnable ("EpcHelper", logLevel);
   LogComponentEnable ("EpcEnbApplication", logLevel);
+  LogComponentEnable ("EpcMmeApplication", logLevel);
+  LogComponentEnable ("EpcPgwApplication", logLevel);
+  LogComponentEnable ("EpcSgwApplication", logLevel);
   LogComponentEnable ("LteEnbRrc", logLevel);
 
 
--- src/lte/examples/lena-x2-handover.cc
+++ src/lte/examples/lena-x2-handover.cc
@@ -127,6 +127,9 @@ main (int argc, char *argv[])
   // LogComponentEnable ("LteHelper", logLevel);
   // LogComponentEnable ("EpcHelper", logLevel);
   // LogComponentEnable ("EpcEnbApplication", logLevel);
+  // LogComponentEnable ("EpcMmeApplication", logLevel);
+  // LogComponentEnable ("EpcPgwApplication", logLevel);
+  // LogComponentEnable ("EpcSgwApplication", logLevel);
   // LogComponentEnable ("EpcX2", logLevel);
 
   // LogComponentEnable ("LteEnbRrc", logLevel);
