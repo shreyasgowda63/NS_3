405056b9e773e3e963b2fab7a796da571ddf28b3
Steven Smith <smith84@llnl.gov>
2020-10-23 09:17:37 -0700

    Update copyright

--- src/simple-distributed/examples/simple-distributed-channel-scaling.cc
+++ src/simple-distributed/examples/simple-distributed-channel-scaling.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- *  Copyright 2018. Lawrence Livermore National Security, LLC.
+ *  Copyright 2020. Lawrence Livermore National Security, LLC.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Steven Smith <smith84@llnl.gov>
+ *
  */
 
 #include "ns3/arp-cache.h"
