d895615dc60a21ecce41c9bc23bec85d1d0ac905
adnanrashidpk <adnanrashidpk@gmail.com>
2021-05-17 19:14:04 +0200

    Building Scripts

--- scratch/withoutsixlowpan-mesh-example.cc
+++ scratch/withoutsixlowpan-mesh-example.cc
@@ -32,7 +32,7 @@
  *          +---------+      +---------+      +---------+
  *              ||               ||               ||
  *               ================   ===============
- *
+ * ./waf --run "scratch/withoutsixlowpan-mesh-example.cc --Mesh --Ping=6LN --LLA --StopTime=2000 --Interval=100"
  */
 
 #include <fstream>
@@ -50,6 +50,7 @@
 #include "ns3/energy-module.h"
 #include "ns3/wifi-module.h"
 #include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
 
 using namespace ns3;
 using namespace std;
@@ -57,43 +58,57 @@ using namespace std;
 
 uint32_t pktCount = 0;
 uint64_t pktTotalSize = 0;
-uint32_t cicle = 10;
-uint32_t nowMax = 1;
+uint32_t ackCount = 0;
+uint64_t ackTotalSize = 0;
+uint32_t unkCount = 0;
+uint64_t unkTotalSize = 0;
+
+std::map<uint8_t, uint32_t> icmpTypeCount;
+uint32_t udpCount = 0;
+uint32_t otherL4Count = 0;
+
+void
+PrintResults (Time interval)
+{
+  std::cout << Now().GetSeconds () << "\t";
+  std::cout << pktCount << "\t" << pktTotalSize << "\t";
+  std::cout << ackCount << "\t" << ackTotalSize << "\t";
+  std::cout << unkCount << "\t" << unkTotalSize << std::endl;
+
+  pktCount = 0;
+  pktTotalSize = 0;
+  ackCount = 0;
+  ackTotalSize = 0;
+  unkCount = 0;
+  unkTotalSize = 0;
+
+  Simulator::Schedule (interval, &PrintResults, interval);
+}
+
 
 void
 PhyCallback (std::string path, Ptr<const Packet> packet)
 {
+  LrWpanMacHeader lrWpanHdr;
+  Ptr<Packet> pktCpy = packet->Copy ();
+  pktCpy->RemoveHeader (lrWpanHdr);
 
-  uint32_t nowMaxLocal = std::floor (Now().GetSeconds () / 10) + 1;
-
-  if ( nowMaxLocal != nowMax)
+  if (lrWpanHdr.IsAcknowledgment ())
     {
-      if (pktCount)
-//        std::cout << "Time " << "\t" << "Packet Count " << "\t" << " Total Size "<< std::endl;
-      std::cout << (nowMax)*10 << "\t" << pktCount << "\t" << pktTotalSize << std::endl;
-      nowMax = nowMaxLocal;
-
-      pktCount = 0;
-      pktTotalSize = 0;
+      ackCount ++;
+      ackTotalSize += packet->GetSize ();
     }
-  pktCount ++;
-  pktTotalSize += packet->GetSize ();
-
-//  if (Now().GetSeconds () > 10)
-//    std::cout << Now ().As (Time::S) << " Tx packet of size " << packet->GetSize () << " - " << *packet << std::endl;
-
-//  std::cout << Now ().As (Time::S) << " - " << packet->GetSize () << std::endl;
-
-//  LrWpanMacHeader lrWpanHdr;
-//  Ptr<Packet> pktCpy = packet->Copy ();
-//  pktCpy->RemoveHeader (lrWpanHdr);
-//
-//  if (lrWpanHdr.IsAcknowledgment ())
-//    std::cout << Now ().As (Time::S)<< " Tx ACK of size " << packet->GetSize () << std::endl;
-//  else if (lrWpanHdr.IsData ())
-//    std::cout << Now ().As (Time::S) << " Tx packet of size " << packet->GetSize () << " - " << *pktCpy << std::endl;
-//  else
-//    std::cout << Now ().As (Time::S) << " Tx OTHER of size " << packet->GetSize () << std::endl;
+  else if (lrWpanHdr.IsData ())
+    {
+      pktCount ++;
+      pktTotalSize += packet->GetSize ();
+    }
+  else
+    {
+      unkCount ++;
+      unkTotalSize += packet->GetSize ();
+      std::cout << *packet << std::endl;
+    }
 }
 
 // Here you see only the packets that IP did send to 6LoWPAN.
@@ -103,23 +118,45 @@ PhyCallback (std::string path, Ptr<const Packet> packet)
 void
 SixLowCallback (std::string path, Ptr<const Packet> packet, Ptr<SixLowPanNetDevice> netDev, uint32_t index)
 {
-//
-//  std::cout << "adnan" <<path << std::endl;
-//  std::cout << Now ().As (Time::S) << " Tx something of size (Packets that IP did send to 6LoWPAN) " << packet->GetSize () << " - " << *packet << std::endl;
+  Ipv6Header ipv6Hdr;
+  Ptr<Packet> pktCpy = packet->Copy ();
+  pktCpy->RemoveHeader (ipv6Hdr);
+  if (ipv6Hdr.GetNextHeader () == UdpL4Protocol::PROT_NUMBER)
+    {
+      udpCount ++;
+    }
+  else if (ipv6Hdr.GetNextHeader () == Icmpv6L4Protocol::PROT_NUMBER)
+    {
+      Icmpv6Header icmpHdr;
+      pktCpy->RemoveHeader (icmpHdr);
+      icmpTypeCount[icmpHdr.GetType ()] ++;
+    }
+  else
+    {
+      otherL4Count ++;
+    }
+
+//std::cout << Now ().As (Time::S) << " Tx something of size (Packets that IP did send to 6LoWPAN) " << packet->GetSize () << " - " << *packet << std::endl;
 }
 
 int main (int argc, char** argv)
 {
 	bool useMeshUnder = false;
+	bool useLLA = false;
+	bool useGUA = false;
 	std::string useUdpFrom = "";
 	std::string usePingOn = "";
-	double stopTime = 50000;
+	double stopTime;
+	Time interval = Seconds (100);
 
 	CommandLine cmd;
 	cmd.AddValue ("Mesh", "Use mesh-under in the network", useMeshUnder);
 	cmd.AddValue ("Udp", "Send one UDP packet from (6LBR, 6LN, nothing)", useUdpFrom);
 	cmd.AddValue ("Ping", "Install Ping app on (6LBR, 6LN, nothing)", usePingOn);
+	cmd.AddValue ("LLA", "Use link-local addresses for the communication", useLLA);
+	cmd.AddValue ("GUA", "Use global addresses for the communication", useGUA);
 	cmd.AddValue ("StopTime", "Simulation stop time (seconds)", stopTime);
+	cmd.AddValue ("Interval", "Sampling interval", interval);
 	cmd.Parse (argc, argv);
 
 	if (useMeshUnder)
@@ -190,56 +227,126 @@ int main (int argc, char** argv)
       else
         iic = ipv6.AssignWithoutAddress (devices.Get(var));
     }
-//  uint32_t packetSize = 10;
-//  uint32_t maxPacketCount = 500;
-//  Time interPacketInterval = Seconds (1.);
-//  Ping6Helper ping6;
-//
-//  //************Communication from 6LN(n1)---to---6LN(n2)************
-//
-//  //GUA-to-GUA---------------------------------working
-//    ping6.SetLocal ("2001::ff:fe00:2");
-//    ping6.SetRemote ("2001::ff:fe00:3");
-//
-//  //LLA-to-LLA---------------------------------Working
-////    ping6.SetLocal ("fe80::ff:fe00:2");
-////    ping6.SetRemote ("fe80::ff:fe00:3");
-//
-//  //GUA-to-LLA---------------------------------working
-////    ping6.SetLocal ("2001::ff:fe00:2");
-////    ping6.SetRemote ("fe80::ff:fe00:3");
-//
-//  //LLA-to-GUA---------------------------------Working
-////    ping6.SetLocal ("fe80::ff:fe00:2");
-////    ping6.SetRemote ("2001::ff:fe00:3");
-//
-//  //************Communication from 6LN(n2)---to---6LN(n1)************
-//
-//  // GUA-to-GUA---------------------------------Working
-////    ping6.SetLocal ("2001::ff:fe00:3");
-////    ping6.SetRemote ("2001::ff:fe00:2");
-//
-//  //LLA-to-LLA---------------------------------Working
-////    ping6.SetLocal ("fe80::ff:fe00:3");
-////    ping6.SetRemote ("fe80::ff:fe00:2");
-//
-//  //GUA-to-LLA---------------------------------Working
-////    ping6.SetLocal ("2001::ff:fe00:3");
-////    ping6.SetRemote ("fe80::ff:fe00:2");
-//
-//  //LLA-to-GUA---------------------------------Working
-////    ping6.SetLocal ("fe80::ff:fe00:3");
-////    ping6.SetRemote ("2001::ff:fe00:2");
-//
-//
-//  ping6.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
-//  ping6.SetAttribute ("Interval", TimeValue (interPacketInterval));
-//  ping6.SetAttribute ("PacketSize", UintegerValue (packetSize));
-//  ApplicationContainer apps = ping6.Install (lo_nodes.Get (1)); // always the SetLocal node index
-//
-//  apps.Start (Seconds (5.0));
-//  apps.Stop (Seconds (100.0));
+  //*********************************ICMPV6 Ping testing*********************************
+    if (usePingOn != "")
+      {
+        uint32_t packetSize = 10;
+        uint32_t maxPacketCount = 100;
+        Time interPacketInterval = Seconds (1.);
+        Ping6Helper ping6;
 
+        ping6.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+        ping6.SetAttribute ("Interval", TimeValue (interPacketInterval));
+        ping6.SetAttribute ("PacketSize", UintegerValue (packetSize));
+        ApplicationContainer apps;
+
+        // 6LBR addresses: "2001::ff:fe00:5" - "fe80::ff:fe00:5"
+        // 6LN addresses: "2001::ff:fe00:1" - "fe80::ff:fe00:1"
+
+        if (usePingOn == "6LBR")
+          {
+            if (useGUA!=false)
+              {
+                ping6.SetLocal ("2001::ff:fe00:5");
+                ping6.SetRemote ("2001::ff:fe00:1");
+                apps.Add (ping6.Install (lo_nodes.Get (4)));
+              }
+            else
+              {
+                ping6.SetLocal ("fe80::ff:fe00:5");
+                ping6.SetRemote ("fe80::ff:fe00:1");
+                apps.Add (ping6.Install (lo_nodes.Get (4)));
+              }
+          }
+        else if (usePingOn == "6LN")
+              {
+                if(useGUA!=false)
+                  {
+                    ping6.SetLocal ("2001::ff:fe00:5");
+                    ping6.SetRemote ("2001::ff:fe00:1");
+                    apps.Add (ping6.Install (lo_nodes.Get (4)));
+                  }
+                else
+                  {
+                    ping6.SetLocal ("fe80::ff:fe00:5");
+                    ping6.SetRemote ("fe80::ff:fe00:1");
+                    apps.Add (ping6.Install (lo_nodes.Get (4)));
+                  }
+              }
+        Received a successful address registration for an address that we did already register. Increase the registration timeout
+        else
+          {
+            std::cout << "PING: invalid option\n";
+            exit (0);
+          }
+        apps.Start (Seconds(1.5));
+        apps.Stop (Seconds (stopTime-1));
+      }
+
+    //*********************************UDP testing*********************************
+    if (useUdpFrom != "")
+    {
+    	uint16_t port = 4000;
+    	UdpServerHelper server (port);
+    	ApplicationContainer udpServerApps = server.Install (lo_nodes);
+    	udpServerApps.Start (Seconds (0.0));
+    	udpServerApps.Stop (Seconds(stopTime - 1));
+
+    	uint32_t MaxPacketSize = 12;
+    	Time interPacketInterval = Seconds (0.05);
+    	uint32_t maxPacketCount = 2;
+
+    	// Server IP and port number
+    	UdpClientHelper client;
+    	client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+    	client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+    	client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
+    	ApplicationContainer udpClientApps;
+
+    	if (useUdpFrom == "6LBR")
+    	{
+    		if(useGUA==false)
+    		{
+    			client.SetAttribute ("RemoteAddress", AddressValue (Ipv6Address ("2001::ff:fe00:2")));
+    			client.SetAttribute ("RemotePort", UintegerValue (port));
+    			udpClientApps.Add (client.Install (lo_nodes.Get (0)));
+    		}
+    		else
+    		{
+    			client.SetAttribute ("RemoteAddress", AddressValue (Ipv6Address ("fe80::ff:fe00:2")));
+    			client.SetAttribute ("RemotePort", UintegerValue (port));
+    			udpClientApps.Add (client.Install (lo_nodes.Get (0)));
+    		}
+    	}
+    	else if (useUdpFrom == "6LN")
+    	{
+    		if(useGUA==false)
+    		{
+    			client.SetAttribute ("RemoteAddress", AddressValue (Ipv6Address ("2001::ff:fe00:1")));
+    			client.SetAttribute ("RemotePort", UintegerValue (port));
+    			udpClientApps.Add (client.Install (lo_nodes.Get (1)));
+    		}
+    		else
+    		{
+    			client.SetAttribute ("RemoteAddress", AddressValue (Ipv6Address ("fe80::ff:fe00:1")));
+    			client.SetAttribute ("RemotePort", UintegerValue (port));
+    			udpClientApps.Add (client.Install (lo_nodes.Get (1)));
+    		}
+    	}
+    	else
+    	{
+    		std::cout << "UDP app: invalid option\n";
+    		exit (0);
+    	}
+    	udpClientApps.Start (Seconds (5.0));
+    	udpClientApps.Stop (Seconds(stopTime - 1));
+    }
+
+
+    if (useUdpFrom != "" && usePingOn != "")
+    {
+    	std::cout<< "****------------------Ping or UDP Applications are not running------------------****"<<std::endl;
+    }
   AsciiTraceHelper ascii;
   lrWpanHelper.EnableAsciiAll (ascii.CreateFileStream ("withoutsixlowpan-mesh-example.tr"));
   lrWpanHelper.EnablePcapAll (std::string ("withoutsixlowpan-mesh-example"), true);
@@ -256,12 +363,14 @@ int main (int argc, char** argv)
   Config::Connect ("/NodeList/*/DeviceList/*/$ns3::SixLowPanNetDevice/TxPre",
                     MakeCallback (&SixLowCallback));
 
-  Simulator::Stop (Seconds (50000));
+  Simulator::Schedule (interval, &PrintResults, interval);
+
+  Simulator::Stop (Seconds (stopTime));
   Simulator::Run ();
 
-  Ptr<Packet> foo = Create<Packet> ();
-  PhyCallback ("foobar", foo);
-  std::cout << "End of simulation at " << Now ().As (Time::S) << std::endl;
+//  Ptr<Packet> foo = Create<Packet> ();
+//  PhyCallback ("foobar", foo);
+//  std::cout << "End of simulation at " << Now ().As (Time::S) << std::endl;
 
   Simulator::Destroy ();
 
