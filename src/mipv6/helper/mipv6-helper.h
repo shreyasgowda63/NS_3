b7a5674f6cb0d031583ff1e802815bc63d42ad72
ZakariaHelalArzoo <arzoozakaria@gmail.com>
2021-06-25 18:55:49 +0530

    mipv6: added mipv6 option headers

---
+++ src/mipv6/helper/mipv6-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef MIPV6_HELPER_H
+#define MIPV6_HELPER_H
+
+#include "ns3/mipv6.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* MIPV6_HELPER_H */
+
