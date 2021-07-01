65004fba7685c56a2c463c0a859688183e3832d2
bbojovic <biljana.bojovic@cttc.es>
2021-07-01 08:44:46 +0200

    spectrum: Support passing the TX and RX PhasedAntennaArray objects to spectrum model
    This possibility is needed for the types of spectrum propagation models that need
    to have a pointer to the TX and RX antenna arrays to perform calculations,
    such as e.g., ThreeGppSpectrumPropagationLossModel, which currently has hard
    limitation of having a maximum of 1 antenna array instance per device.
    
    This patch allows having one PhasedArrayModel instance per one instance of
    new PhasedArraySpectrumPhy object which inherits SpectrumPhy, and extends it
    to have a pointer to PhasedArrayModel instance.
    
    Since the ns-3 device can have multiple instances of SpectrumPhy or PhasedArraySpectrumPhy,
    with this changeset, the ns-3 device can also have multiple antenna arrays
    (1 per SpectrumPhy).
    
    ThreeGppSpectrumPropagationLossModel now inherits a new class PhasedArraySpectrumPropagationLossModel.
    Its DoCalcRxPowerSpectralDensity function has now two additional parameters: TX and RX antenna arrays.
    Also, AddDevice function is removed from ThreeGppSpectrumPropagationLossModel, because it is not
    anymore needed to specify for each device its TX/RX antenna, since these are now passed as parameters.
    
    Hence, to use ThreeGppSpectrumPropagationLossModel, when implementing Spectrum PHY features,
    modules should inherit PhasedArraySpectrumPhy type instead of SpectrumPhy,
    and configure PhasedArrayModel for each PhasedArraySpectrumPhy instance.
    
    These instances will be passed by MultiModelSpectrumChannel (one of the core classes in the spectrum module)
    to propagation classes, children of PhasedArraySpectrumPropagationLossModel,
    when it is necessary to DoCalcRxPowerSpectralDensity to callculate the propagation loss.

--- src/spectrum/model/three-gpp-spectrum-propagation-loss-model.h
+++ src/spectrum/model/three-gpp-spectrum-propagation-loss-model.h
@@ -22,7 +22,7 @@
 #ifndef THREE_GPP_SPECTRUM_PROPAGATION_LOSS_H
 #define THREE_GPP_SPECTRUM_PROPAGATION_LOSS_H
 
-#include "ns3/spectrum-propagation-loss-model.h"
+#include "ns3/phased-array-spectrum-propagation-loss-model.h"
 #include <complex.h>
 #include <map>
 #include <unordered_map>
@@ -49,7 +49,7 @@ class ChannelCondition;
  * \see PhasedArrayModel
  * \see ChannelCondition
  */
-class ThreeGppSpectrumPropagationLossModel : public SpectrumPropagationLossModel
+class ThreeGppSpectrumPropagationLossModel : public PhasedArraySpectrumPropagationLossModel
 {
 public:
   /**
@@ -81,14 +81,6 @@ public:
    * \return a pointer to the object implementing the MatrixBasedChannelModel interface
    */
   Ptr<MatrixBasedChannelModel> GetChannelModel() const;
-
-  /**
-   * Add a device-antenna pair
-   * \param n a pointer to the NetDevice
-   * \param a a pointer to the associated PhasedArrayModel
-   */
-  void AddDevice (Ptr<NetDevice> n, Ptr<const PhasedArrayModel> a);
-
 
   /**
    * Sets the value of an attribute belonging to the associated
@@ -127,8 +119,10 @@ public:
    * \return the received PSD
    */
   Ptr<SpectrumValue> DoCalcRxPowerSpectralDensity (Ptr<const SpectrumValue> txPsd,
-                                                           Ptr<const MobilityModel> a,
-                                                           Ptr<const MobilityModel> b) const override;
+                                                   Ptr<const MobilityModel> a,
+                                                   Ptr<const MobilityModel> b,
+                                                   Ptr<const PhasedArrayModel> aPhasedArrayModel,
+                                                   Ptr<const PhasedArrayModel> bPhasedArrayModel) const override;
 
 private:
   /**
@@ -188,7 +182,6 @@ private:
                                           Ptr<const MatrixBasedChannelModel::ChannelMatrix> params,
                                           const Vector &sSpeed, const Vector &uSpeed) const;
 
-  std::unordered_map <uint32_t, Ptr<const PhasedArrayModel> > m_deviceAntennaMap; //!< map containig the <node, antenna> associations
   mutable std::unordered_map < uint32_t, Ptr<const LongTerm> > m_longTermMap; //!< map containing the long term components
   Ptr<MatrixBasedChannelModel> m_channelModel; //!< the model to generate the channel matrix
   
