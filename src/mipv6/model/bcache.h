14387595310a5cafb7926d0ea7b4aecb0342c1f8
ZakariaHelalArzoo <arzoozakaria@gmail.com>
2021-08-18 20:22:36 +0530

    mipv6: cache behaviour defined

--- src/mipv6/model/bcache.h
+++ src/mipv6/model/bcache.h
@@ -22,13 +22,20 @@
 #define B_CACHE_H
 
 
+
+#include <list>
+#include "ns3/nstime.h"
 #include "ns3/node.h"
+#include "ns3/ipv6-address.h"
+#include "ns3/ptr.h"
+#include "ns3/timer.h"
+#include <unordered_map>
 
 namespace ns3 {
 
 /**
  * \class BCache
- * \brief BCache class is associated with Mipv6Ha and Mipv6Cn class. It contain data
+ * \brief BCache class is associated with Mipv6Ha class. It contain data
  * members: CoA, HoA, lifetime, HA address, tunnel interface index, sequence
  * number, BU state, and nonce indices as defined in RFC 6275. To handle the
  * information of multiple MNs and HAs, each entry in the BCache is keyed by
@@ -37,6 +44,7 @@ namespace ns3 {
 class BCache : public Object
 {
 public:
+  class Entry;
 
   /**
    * \brief Get the type identifier.
@@ -54,7 +62,336 @@ public:
    */
   ~BCache ();
 
-  // TODO: define bcache class
+  /**
+   * \brief Lookup entry for an MN.
+   * \param mnhoa MN's home address
+   * \returns the entry of an MN
+   */
+  BCache::Entry * Lookup (Ipv6Address mnhoa);
+
+  /**
+   * \brief Add an entry for an MN.
+   * \param bce binding cache entry
+   */
+  void Add (BCache::Entry *bce);
+
+  /**
+   * \brief remove the entry of an MN.
+   * \param entry MN's entry
+   */
+  void Remove (BCache::Entry *entry);
+
+  /**
+   * \brief Lookup whether the HoA cached in it.
+   * \param shoa solicited home address
+   * \returns status
+   */
+  BCache::Entry * LookupSHoa (Ipv6Address shoa);
+
+  /**
+   * \brief delete all entries in the cache
+   */
+  void Flush ();
+
+  /**
+   * \brief get node.
+   * \returns the node pointer
+   */
+  Ptr<Node> GetNode () const;
+
+  /**
+   * \brief set node.
+   * \param node the node pointer
+   */
+  void SetNode (Ptr<Node> node);
+
+  /**
+   * \brief stores the home prefixes. It makes zero last 64 bit if HA addresses
+   * \param halist the list of Ipv6 addresses of all HA's links
+   */
+  void SetHomePrefixes (std::list<Ipv6Address> halist);
+
+
+  /**
+   * Entry for an MN
+   */
+  class Entry
+  {
+  public:
+    /**
+     * \enum State_e
+     * \brief state of the address
+     */
+    enum State_e
+    {
+      TENTATIVE, /**< Address is tentative, no packet can be sent unless DAD finished */
+      DEPRECATED, /**< Address is deprecated and should not be used */
+      PREFERRED, /**< Preferred address */
+      PERMANENT, /**< Permanent address */
+      HOMEADDRESS, /**< Address is a HomeAddress */
+      TENTATIVE_OPTIMISTIC, /**< Address is tentative but we are optimistic so we can send packet even if DAD is not yet finished */
+      INVALID, /**< Invalid state (after a DAD failed) */
+    };
+    /**
+     * \brief constructor
+     * \param bcache binding cache
+     */
+    Entry (Ptr<BCache> bcache);
+
+    /**
+     * \brief copy of an entry object
+     * \returns an entry object
+     */
+    Entry * Copy ();
+
+
+    /**
+     * \brief whether MN is unreachable
+     * \returns status
+     */
+    bool IsUnreachable () const;
+
+    /**
+     * \brief whether MN is reachable
+     * \returns status
+     */
+    bool IsReachable () const;
+
+    /**
+     * \brief mark MN's reachability status as reachable
+     */
+    void MarkReachable ();
+
+    /**
+     * \brief check against MN's HoA
+     * \param mnhoa MN HoA
+     * \returns whether MN's hoa is matched
+     */
+    bool Match (Ipv6Address mnhoa) const;
+
+    /**
+     * \brief get the CoA of MN
+     * \returns care-of-address of MN
+     */
+    Ipv6Address GetCoa () const;
+
+    /**
+     * \brief set CoA of MN
+     * \param mnhoa MN HoA
+     */
+    void SetCoa (Ipv6Address mnhoa);
+
+    /**
+     * \brief get the HA address of MN
+     * \returns status
+     */
+    Ipv6Address GetHA () const;
+
+    /**
+     * \brief Set the HA address of MN
+     * \param haa HA address
+     */
+    void SetHA (Ipv6Address haa);
+
+    /**
+     * \brief get solicited HoA of MN
+     * \returns solicited HoA
+     */
+    Ipv6Address GetSolicitedHoA () const;
+
+    /**
+     * \brief set solicited HoA of MN
+     * \param shoa solicited HoA
+     */
+    void SetSolicitedHoA (Ipv6Address shoa);
+
+    /**
+     * \brief get HoA of the MN
+     * \return HoA
+     */
+    Ipv6Address GetHoa () const;
+
+    /**
+     * \brief set set HoA of MN
+     * \param hoa home address
+     */
+    void SetHoa (Ipv6Address hoa);
+
+    /**
+     * \brief get tunnel interface index of an MN
+     * \return interface index
+     */
+    int16_t GetTunnelIfIndex () const;
+
+    /**
+     * \brief set tunnel interface index
+     * \param tunnelif tunnel interface index
+     */
+    void SetTunnelIfIndex (int16_t tunnelif);
+
+    /**
+     * \brief get last binding update time of MN
+     * \returns bu time
+     */
+    Time GetLastBindingUpdateTime () const;
+
+    /**
+     * \brief set last binding update time of MN
+     * \param tm last bu time
+     */
+    void SetLastBindingUpdateTime (Time tm);
+
+    /**
+     * \brief get last BU sequence of MN
+     * \returns BU sequence
+     */
+    uint16_t GetLastBindingUpdateSequence () const;
+
+    /**
+     * \brief set last BU sequence
+     * \param seq BU seq. no.
+     */
+    void SetLastBindingUpdateSequence (uint16_t seq);
+
+    /**
+     * \brief get next pointer
+     * \return next pointer
+     */
+    Entry * GetNext () const;
+
+    /**
+     * \brief set next pointer
+     * \param entry next pointer
+     */
+    void SetNext (Entry *entry);
+
+    /**
+     * \brief get the CoA MN hold before current BU request
+     * \returns the old CoA
+     */
+    Ipv6Address GetOldCoa () const;
+
+    /**
+     * \brief set binding cache entry state of MN
+     * \param state the state of that entry
+     */
+    void SetState (BCache::Entry::State_e state);
+
+    /**
+     * \brief get binding cache entry state of MN
+     * \return cache entry state
+     */
+    BCache::Entry::State_e GetState ();
+
+  private:
+
+    /**
+     * \brief The BCache object which holds this entry
+     */
+    Ptr<BCache> m_bCache;
+
+    /**
+     * \brief The enum definition to define the state of this entry
+     */
+    enum BCacheState_e
+    {
+      UNREACHABLE,
+      DEREGISTERING,
+      REGISTERING,
+      REACHABLE
+    };
+
+    /**
+     * \brief The state of this entry
+     */
+    BCacheState_e m_state;
+
+    /**
+     * \brief The home agent address of the MN
+     */
+    Ipv6Address m_haa;
+
+    /**
+     * \brief The CoA of the MN
+     */
+    Ipv6Address m_coa;
+
+    /**
+     * \brief The HoA of the MN
+     */
+    Ipv6Address m_hoa;
+
+    /**
+     * \brief The solicited HoA of the MN
+     */
+    Ipv6Address m_shoa;
+
+    /**
+     * \brief The tunnel interface index of the MN
+     */
+    int16_t m_tunnelIfIndex;
+
+    /**
+     * \brief The last binding update time of the MN
+     */
+    Time m_lastBindingUpdateTime;
+
+    /**
+     * \brief The last binding update sequence of the MN
+     */
+    uint16_t m_lastBindingUpdateSequence;
+
+    /**
+     * \brief The next link of the entry
+     */
+    Entry *m_next;
+
+    //internal
+
+    /**
+     * \brief The Old CoA which was previosly bound with HoA
+     */
+    Ipv6Address m_oldCoa;
+
+    /**
+     * \brief The state of the address
+     */
+    State_e m_addrstate;
+
+  };
+
+protected:
+
+private:
+
+  /**
+   * \brief The hashmap of entries keyed by home addresses
+   */
+  typedef std::unordered_map<Ipv6Address, BCache::Entry *, Ipv6AddressHash> bCache;
+
+  /**
+   * \brief The hashmap iterator of entries keyed by home addresses
+   */
+  typedef std::unordered_map<Ipv6Address, BCache::Entry *, Ipv6AddressHash>::iterator BCacheI;
+
+  void DoDispose ();
+
+  /**
+   * \brief The BCache
+   */
+  bCache m_bCache;
+
+  /**
+   * \brief The home agent address list
+   */
+  std::list<Ipv6Address> m_HaaList;
+
+  /**
+   * \brief The home agent prefix list
+   */
+  std::list<Ipv6Address> m_HomePrefixList;
+
+  Ptr<Node> m_node; //!< The node pointer of the HA/CN
 };
 
 }
--- src/mipv6/model/blist.h
+++ src/mipv6/model/blist.h
@@ -31,10 +31,6 @@
 
 namespace ns3 {
 
-/**
- * \class BList
- * \brief Binding List of Mobile Node
- */
 class BList : public Object
 {
 public:
@@ -52,13 +48,404 @@ public:
    * \brief destructor
    */
   ~BList ();
-  
+
   /**
-   * \brief Return the home address
-   * \return HoA
+   * \brief get the node pointer.
+   * \returns the node pointer 
+   */
+  Ptr<Node> GetNode () const;
+
+  /**
+   * \brief set the node pointer.
+   * \param node the node pointer 
+   */
+  void SetNode (Ptr<Node> node);
+
+
+  /**
+   * \brief whether the home agent is unreachable.
+   * \returns the unreachability status of the home agent 
+   */
+  bool IsHomeUnreachable () const;
+
+  /**
+   * \brief whether the home agent is updating now.
+   * \returns the updating status of the home agent 
+   */
+  bool IsHomeUpdating () const;
+
+  /**
+   * \brief whether the MN currently performing BU process.
+   * \returns the status of the home refreshment 
+   */
+  bool IsHomeRefreshing () const;
+
+  /**
+   * \brief whether the home agent is reachable.
+   * \returns the reachability status of the home agent 
+   */
+  bool IsHomeReachable () const;
+
+  /**
+   * \brief mark the reachability status of the home agent as unreachable.
+   */
+  void MarkHomeUnreachable ();
+
+  /**
+   * \brief mark the updating status of the home agent as updating.
+   */
+  void MarkHomeUpdating ();
+
+  /**
+   * \brief mark the refreshing status of the home agent as refreshing.
+   */
+  void MarkHomeRefreshing ();
+
+  /**
+   * \brief mark the reachability status of the home agent as reachable.
+   */
+  void MarkHomeReachable ();
+
+  //timer processing
+
+  /**
+   * \brief If BU transmission failed it starts the BU transmission process.
+   */
+  void StartHomeRetransTimer ();
+
+  /**
+   * \brief If BU transmission succeded or, time out happens, it stops the retransmission timer.
+   */
+  void StopHomeRetransTimer ();
+
+  /**
+   * \brief not used.
+   */
+  void StartHomeReachableTimer ();
+
+  /**
+   * \brief not used.
+   */
+  void StopHomeReachableTimer ();
+
+  /**
+   * \brief not used.
+   */
+  void StartHomeRefreshTimer ();
+
+  /**
+   * \brief not used.
+   */
+  void StopHomeRefreshTimer ();
+
+  /**
+   * \brief not used.
+   */
+  void FunctionHomeRetransTimeout ();
+
+  /**
+   * \brief not used.
+   */
+  void FunctionHomeReachableTimeout ();
+
+  /**
+   * \brief not used.
+   */
+  void FunctionHomeRefreshTimeout ();
+
+  /**
+   * \brief not used.
+   */
+  Time GetHomeReachableTime () const;
+
+  /**
+   * \brief not used.
+   */
+  void SetHomeReachableTime (Time tm);
+
+
+  /**
+   * \brief no of retransmission tried.
+   * \return home BU retry count
+   */
+  uint8_t GetHomeRetryCount () const;
+
+  /**
+   * \brief increase home retransmission counter.
+   */
+  void IncreaseHomeRetryCount ();
+
+  /**
+   * \brief reset home retransmission counter.
+   */
+  void ResetHomeRetryCount ();
+
+  /**
+   * \brief get home bu initial lifetime.
+   * \return home bu initial lifetime
+   */
+  Time GetHomeInitialLifeTime () const;
+  /**
+   * \brief set home bu initial lifetime.
+   * \param tm home bu initial lifetime
+   */
+  void SetHomeInitialLifeTime (Time tm);
+
+  /**
+   * \brief get home bu remaining lifetime.
+   * \return home bu remaining lifetime
+   */
+  Time GetHomeRemainingLifeTime () const;
+
+  /**
+   * \brief get last home bu time.
+   * \return last home bu lifetime
+   */
+  Time GetLastHomeBindingUpdateTime () const;
+  /**
+   * \brief set last home bu time.
+   * \param tm last home bu time
+   */
+  void SetLastHomeBindingUpdateTime (Time tm);
+
+  /**
+   * \brief get max home bu sequence.
+   * \return max home bu sequence
+   */
+  uint16_t GetHomeMaxBindingUpdateSequence () const;
+  /**
+   * \brief set max home bu sequence.
+   * \param seq max home bu sequence
+   */
+  void SetHomeMaxBindingUpdateSequence (uint16_t seq);
+
+  /**
+   * \brief get last home bu sequence.
+   * \return last home bu sequence
+   */
+  uint16_t GetHomeLastBindingUpdateSequence () const;
+
+  /**
+   * \brief set last home bu sequence.
+   * \param seq last home bu sequence
+   */
+  void SetHomeLastBindingUpdateSequence (uint16_t seq);
+
+  /**
+   * \brief get last home bu time.
+   * \return last home bu time
+   */
+  Time GetHomeLastBindingUpdateTime () const;
+
+  /**
+   * \brief set last home bu time.
+   * \param tm last home bu time
+   */
+  void SetHomeLastBindingUpdateTime (Time tm);
+
+  /**
+   * \brief get tunnel interface index.
+   * \return tunnel interface index
+   */
+  int16_t GetTunnelIfIndex () const;
+
+  /**
+   * \brief set tunnel interface index.
+   * \param tunnelif tunnel interface index
+   */
+  void SetTunnelIfIndex (int16_t tunnelif);
+
+  /**
+   * \brief set home address.
+   * \param hoa home address
+   */
+  void SetHoa (Ipv6Address hoa);
+  /**
+   * \brief get home address.
+   * \return home address
    */
   Ipv6Address GetHoa () const;
+  /**
+   * \brief get care-of-address.
+   * \return care-of-address
+   */
+  Ipv6Address GetCoa () const;
+  /**
+   * \brief set care-of-address.
+   * \param addr care-of-address
+   */
+  void SetCoa (Ipv6Address addr);
+  /**
+   * \brief set home agent address.
+   * \param ha home agent address
+   */
+  void SetHA (Ipv6Address ha);
+  /**
+   * \brief get home agent address.
+   * \return home agent address
+   */
+  Ipv6Address GetHA () const;
 
+  /**
+   * \brief get home agent address list.
+   * \return home agent address list
+   */
+  std::list<Ipv6Address> GetHomeAgentList () const;
+  /**
+   * \brief set home agent address list.
+   * \param haalist home agent address list
+   */
+  void SetHomeAgentList (std::list<Ipv6Address> haalist);
+
+  /**
+   * \brief get home BU flag.
+   * \return home BU flag
+   */
+  bool GetHomeBUFlag () const;
+  /**
+   * \brief set home BU flag.
+   * \param f home BU flag
+   */
+  void SetHomeBUFlag (bool f);
+
+  /**
+   * \brief get home BU packet.
+   * \return home BU packet
+   */
+  Ptr<Packet> GetHomeBUPacket () const;
+  /**
+   * \brief set home BU packet.
+   * \param pkt home BU packet
+   */
+  void SetHomeBUPacket (Ptr<Packet> pkt);
+
+  /**
+   * \brief flush
+   */
+  void Flush ();
+
+  /**
+   * \brief set home address registered flag.
+   * \param flag home address registered flag
+   */
+  void SetHomeAddressRegistered (bool flag);
+
+  /**
+   * \brief check home address registered flag.
+   * \return home address registered flag
+   */
+  bool IsHomeAddressRegistered ();
+
+
+
+private:
+
+  /**
+   * \enum BindingUpdateState_e
+   * \brief binding update state
+   */
+  enum BindingUpdateState_e
+  {
+    UNREACHABLE,
+    UPDATING,
+    REFRESHING,
+    REACHABLE,
+  };
+
+  /**
+   * \brief binding update state variable
+   */
+  BindingUpdateState_e m_hstate;
+
+  /**
+   * \brief tunnel interface index
+   */
+  int16_t m_tunnelIfIndex;
+
+  /**
+   * \brief home bu packet
+   */
+  Ptr<Packet> m_hpktbu;
+
+  /**
+   * \brief home initial lifetime
+   */
+  Time m_hinitiallifetime;
+
+  /**
+   * \brief home last binding update sequence
+   */
+  uint16_t m_hlastBindingUpdateSequence;
+
+  /**
+   * \brief home bu flag
+   */
+  bool m_hflag;
+
+  /**
+   * \brief home bu last sent
+   */
+  Time m_hbulastsent;
+
+  /**
+   * \brief home address
+   */
+  Ipv6Address m_hoa;
+
+  /**
+   * \brief CoA
+   */
+  Ipv6Address m_coa;
+
+  /**
+   * \brief home agent address
+   */
+  Ipv6Address m_ha;
+
+  /**
+   * \brief home agent address list
+   */
+  std::list<Ipv6Address> m_HaaList;
+
+  /**
+   * \brief home reachable time
+   */
+  Time m_hreachableTime;
+
+  /**
+   * \brief home retransmission timer
+   */
+  Timer m_hretransTimer;
+
+  /**
+   * \brief home reachable timer
+   */
+  Timer m_hreachableTimer;
+
+  /**
+   * \brief home refresh timer
+   */
+  Timer m_hrefreshTimer;
+
+  /**
+   * \brief home bu retry count
+   */
+  uint8_t m_hretryCount;
+
+  /**
+   * \brief home address registered flag
+   */
+  bool m_HomeAddressRegisteredFlag;
+
+  /**
+   * \brief Dispose this object.
+   */
+  void DoDispose ();
+
+  /**
+   * \brief the MN.
+   */
+  Ptr<Node> m_node;
 };
 
 } /* ns3 */
