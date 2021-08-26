526bf00af76e638cf140f838a5bd712116a564e6
nikih94 <nikihuni@gmail.com>
2021-08-26 13:56:14 +0200

    Changed LOG_ERROR, Added enum to define addressType, changed license.

--- src/internet-apps/model/onion-routing.cc
+++ src/internet-apps/model/onion-routing.cc
@@ -1,33 +1,26 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 
+
 /*
-* MIT License
+* Copyright (c) 2020 DLTLT 
 *
-* Copyright (c) 2020 DLTLT
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation;
 *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 *
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *
 * Author: Niki Hrovatin <niki.hrovatin@famnit.upr.si>
-*
 */
+
 
 
 
@@ -55,11 +48,10 @@ OnionRouting::GetTypeId (void)
 
 
 
-OnionRouting::OnionRouting (uint16_t keySize,uint16_t sealPadding, uint16_t addressSize)
+OnionRouting::OnionRouting (uint16_t sealPadding,  AddressType addressType)
 {
-  this->m_keySize = keySize;
   this->m_sealPadding = sealPadding;
-  this->m_addressSize = addressSize;       //4B for IPv4
+  this->m_addressSize = (uint16_t) addressType;
 }
 
 
@@ -72,7 +64,7 @@ OnionRouting::BuildOnion (uint8_t * cipher, uint8_t ** route, uint8_t ** keys,ui
   if (routeLen < 4)
     {
 
-      NS_LOG_ERROR ("Route is to short, need at least 3 intermediate hops.");
+      NS_LOG_LOGIC ("Route is too short, need at least 3 intermediate hops.");
       return 1;
 
     }
@@ -100,7 +92,7 @@ OnionRouting::BuildOnion (uint8_t * cipher, uint8_t ** route, uint8_t ** keys, u
   if (routeLen < 4)
     {
 
-      NS_LOG_ERROR ("Route is to short, need at least 3 intermediate hops.");
+      NS_LOG_LOGIC ("Route is too short, need at least 3 intermediate hops.");
       return 1;
 
     }
@@ -129,7 +121,7 @@ OnionRouting::BuildOnion (uint8_t * cipher, uint8_t ** route, uint8_t ** keys, u
   if (routeLen < 4)
     {
 
-      NS_LOG_ERROR ("Route is to short, need at least 3 intermediate hops.");
+      NS_LOG_LOGIC ("Route is too short, need at least 3 intermediate hops.");
       return 1;
 
     }
@@ -157,7 +149,7 @@ OnionRouting::BuildOnion (uint8_t * cipher, uint8_t ** route, uint8_t ** keys, u
   if (routeLen < 4)
     {
 
-      NS_LOG_ERROR ("Route is to short, need at least 3 intermediate hops.");
+      NS_LOG_LOGIC ("Route is too short, need at least 3 intermediate hops.");
       return 1;
 
     }
