de5399872283e78edde4504ae63d74b1892adcc7
Caleb Bowers <caleb.bowers@nrl.navy.mil>
2021-08-27 15:34:48 -0400

    Now I restrict duplicate detection to multicast

--- src/internet/model/ipv4-l3-protocol.cc
+++ src/internet/model/ipv4-l3-protocol.cc
@@ -804,7 +804,7 @@ Ipv4L3Protocol::Send (Ptr<Packet> packet,
       NS_LOG_LOGIC ("Ipv4L3Protocol::Send case 1b:  passed in with route and valid gateway");
       int32_t interface = GetInterfaceForDevice (route->GetOutputDevice ());
       m_sendOutgoingTrace (ipHeader, packet, interface);
-      if(m_enableDpd)
+      if(m_enableDpd && ipHeader.GetDestination ().IsMulticast ())
       {
         UpdateDuplicate (packet, ipHeader);
       }
