b99333750a3a01565f8ea8de91f212eb3a49835d
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2021-12-07 23:31:48 -0300

    build: Handle relative output paths

--- buildsupport/macros_and_definitions.cmake
+++ buildsupport/macros_and_definitions.cmake
@@ -63,36 +63,40 @@ if("${NS3_OUTPUT_DIRECTORY}" STREQUAL "")
   set(CMAKE_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/build) # default output
                                                           # folder
 else()
-  # Check if we can create that directory
-  if(NOT (EXISTS ${NS3_OUTPUT_DIRECTORY}))
+  # Check if NS3_OUTPUT_DIRECTORY is a relative path
+  set(absolute_ns3_output_directory "${NS3_OUTPUT_DIRECTORY}")
+  if(NOT IS_ABSOLUTE ${NS3_OUTPUT_DIRECTORY})
+      set(absolute_ns3_output_directory "${PROJECT_SOURCE_DIR}/${NS3_OUTPUT_DIRECTORY}")
+  endif()
+  if(NOT (EXISTS ${absolute_ns3_output_directory}))
     message(
       STATUS
         "User-defined output directory \"${NS3_OUTPUT_DIRECTORY}\" doesn't exist. Trying to create it"
     )
-    file(MAKE_DIRECTORY ${NS3_OUTPUT_DIRECTORY})
-    if(NOT (EXISTS ${NS3_OUTPUT_DIRECTORY}))
+    file(MAKE_DIRECTORY ${absolute_ns3_output_directory})
+    if(NOT (EXISTS ${absolute_ns3_output_directory}))
       message(
         FATAL_ERROR
-          "User-defined output directory \"${NS3_OUTPUT_DIRECTORY}\" could not be created. "
+          "User-defined output directory \"${absolute_ns3_output_directory}\" could not be created. "
           "Try changing the value of NS3_OUTPUT_DIRECTORY"
       )
     endif()
 
     # If this directory is not inside the ns-3-dev folder, alert users tests may
     # break
-    if(NOT ("${NS3_OUTPUT_DIRECTORY}" MATCHES "${PROJECT_SOURCE_DIR}"))
+    if(NOT ("${absolute_ns3_output_directory}" MATCHES "${PROJECT_SOURCE_DIR}"))
       message(
         WARNING
-          "User-defined output directory \"${NS3_OUTPUT_DIRECTORY}\" is outside "
+          "User-defined output directory \"${absolute_ns3_output_directory}\" is outside "
           " of the ns-3 directory ${PROJECT_SOURCE_DIR}, which will break some tests"
       )
     endif()
   endif()
   message(
     STATUS
-      "User-defined output directory \"${NS3_OUTPUT_DIRECTORY}\" will be used"
+      "User-defined output directory \"${absolute_ns3_output_directory}\" will be used"
   )
-  set(CMAKE_OUTPUT_DIRECTORY ${NS3_OUTPUT_DIRECTORY})
+  set(CMAKE_OUTPUT_DIRECTORY ${absolute_ns3_output_directory})
 endif()
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_OUTPUT_DIRECTORY}/lib)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_OUTPUT_DIRECTORY}/lib)
