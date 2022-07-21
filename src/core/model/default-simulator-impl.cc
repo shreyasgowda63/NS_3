3b99f80c43197dde1db829d91587d766183730a7
Torben Petersen <petersen@ibr.cs.tu-bs.de>
2022-07-21 14:39:26 +0200

    src/core: Changes ASSERT to ABORT_MSG_UNLESS, places it before the increment, changes condition, place check before increment

--- src/core/model/default-simulator-impl.cc
+++ src/core/model/default-simulator-impl.cc
@@ -179,10 +179,10 @@ DefaultSimulatorImpl::ProcessEventsWithContext (void)
       ev.key.m_ts = m_currentTs + event.timestamp;
       ev.key.m_context = event.context;
       ev.key.m_uid = m_uid;
+
+      NS_ABORT_MSG_UNLESS( ev.key.m_uid != ULLONG_MAX, "Event ID at max. Stop to prevent overflow!");
+
       m_uid++;
-
-      NS_ASSERT_MSG( ev.key.m_uid < m_uid, "Event ID overflow!!!!");
-
       m_unscheduledEvents++;
       m_events->Insert (ev);
     }
@@ -239,10 +239,10 @@ DefaultSimulatorImpl::Schedule (Time const &delay, EventImpl *event)
   ev.key.m_ts = (uint64_t) tAbsolute.GetTimeStep ();
   ev.key.m_context = GetContext ();
   ev.key.m_uid = m_uid;
+
+  NS_ABORT_MSG_UNLESS( ev.key.m_uid != ULLONG_MAX, "Event ID at max. Stop to prevent overflow!");
+
   m_uid++;
-
-  NS_ASSERT_MSG( ev.key.m_uid < m_uid, "Event ID overflow!!!!");
-
   m_unscheduledEvents++;
   m_events->Insert (ev);
   return EventId (event, ev.key.m_ts, ev.key.m_context, ev.key.m_uid);
@@ -261,10 +261,10 @@ DefaultSimulatorImpl::ScheduleWithContext (uint32_t context, Time const &delay, 
       ev.key.m_ts = (uint64_t) tAbsolute.GetTimeStep ();
       ev.key.m_context = context;
       ev.key.m_uid = m_uid;
+
+      NS_ABORT_MSG_UNLESS( ev.key.m_uid != ULLONG_MAX, "Event ID at max. Stop to prevent overflow!");
+
       m_uid++;
-
-      NS_ASSERT_MSG( ev.key.m_uid < m_uid, "Event ID overflow!!!!");
-
       m_unscheduledEvents++;
       m_events->Insert (ev);
     }
@@ -300,6 +300,9 @@ DefaultSimulatorImpl::ScheduleDestroy (EventImpl *event)
 
   EventId id (Ptr<EventImpl> (event, false), m_currentTs, 0xffffffff, 2);
   m_destroyEvents.push_back (id);
+
+  NS_ABORT_MSG_UNLESS( ev.key.m_uid != ULLONG_MAX, "Event ID at max. Stop to prevent overflow!");
+
   m_uid++;
   return id;
 }
