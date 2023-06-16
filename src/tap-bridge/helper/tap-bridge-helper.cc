1a2b3328cdaa2765e0eae65bc92a00a863ef98e8
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2023-06-16 13:09:38 +0200

    tap-bridge: Remove unused gateway

--- src/tap-bridge/helper/tap-bridge-helper.cc
+++ src/tap-bridge/helper/tap-bridge-helper.cc
@@ -38,7 +38,6 @@ TapBridgeHelper::TapBridgeHelper(Ipv4Address gateway)
 {
     NS_LOG_FUNCTION_NOARGS();
     m_deviceFactory.SetTypeId("ns3::TapBridge");
-    SetAttribute("Gateway", Ipv4AddressValue(gateway));
     SetAttribute("Mode", EnumValue(TapBridge::CONFIGURE_LOCAL));
 }
 
--- src/tap-bridge/model/tap-creator.cc
+++ src/tap-bridge/model/tap-creator.cc
@@ -139,12 +139,7 @@ CreateInetAddress(uint32_t networkOrder)
 }
 
 static int
-CreateTap(const char* dev,
-          const char* gw,
-          const char* ip,
-          const char* mac,
-          const char* mode,
-          const char* netmask)
+CreateTap(const char* dev, const char* ip, const char* mac, const char* mode, const char* netmask)
 {
     //
     // Creation and management of Tap devices is done via the tun device
@@ -225,7 +220,6 @@ main(int argc, char* argv[])
 {
     int c;
     char* dev = (char*)"";
-    char* gw = nullptr;
     char* ip = nullptr;
     char* mac = nullptr;
     char* netmask = nullptr;
@@ -234,15 +228,12 @@ main(int argc, char* argv[])
 
     opterr = 0;
 
-    while ((c = getopt(argc, argv, "vd:g:i:m:n:o:p:")) != -1)
+    while ((c = getopt(argc, argv, "vd:i:m:n:o:p:")) != -1)
     {
         switch (c)
         {
         case 'd':
             dev = optarg; // name of the new tap device
-            break;
-        case 'g':
-            gw = optarg; // gateway address for the new device
             break;
         case 'i':
             ip = optarg; // ip address of the new device
@@ -272,14 +263,6 @@ main(int argc, char* argv[])
     // create the device for us.  This name is given in dev
     //
     LOG("Provided Device Name is \"" << dev << "\"");
-
-    //
-    // We have got to be able to provide a gateway to the external Linux host
-    // so it can talk to the ns-3 network.  This ip address is provided in
-    // gw.
-    //
-    ABORT_IF(gw == nullptr, "Gateway Address is a required argument", 0);
-    LOG("Provided Gateway Address is \"" << gw << "\"");
 
     //
     // We have got to be able to assign an IP address to the tap device we are
@@ -335,7 +318,7 @@ main(int argc, char* argv[])
     // us to execute the following code:
     //
     LOG("Creating Tap");
-    int sock = CreateTap(dev, gw, ip, mac, operatingMode, netmask);
+    int sock = CreateTap(dev, ip, mac, operatingMode, netmask);
     ABORT_IF(sock == -1, "main(): Unable to create tap socket", 1);
 
     //
