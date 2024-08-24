c146821897d049cb0d56e0f2e811bb4a27f4df70
Kavya Bhat <kavyabhat@gmail.com>
2024-08-24 10:03:05 -0400

    internet-apps: Add copyright, sort lists alphabetically

--- src/internet-apps/test/dhcp6-test.cc
+++ src/internet-apps/test/dhcp6-test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c)
+ * Copyright (c) 2024 NITK Surathkal
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -13,6 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Kavya Bhat <kavyabhat@gmail.com>
  *
  */
 
